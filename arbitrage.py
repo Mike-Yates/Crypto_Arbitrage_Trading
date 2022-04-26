4d7c5cb9f48d86f13df8067af53bbacdc16b53ea
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-04-26 16:11:51 -0400

    last thing to do is properly calculate gas fees

--- arbitrage.py
+++ arbitrage.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 from hexbytes import HexBytes
 from config import *
+import math 
 #import config 
 
 def performArbitrage(): 
@@ -12,7 +13,6 @@ def performArbitrage():
     print(w3.isConnected())
     
     abi = dex_abi # ABI for DEX.sol
-    
 
     dex_values = [{}, {}, {}, {}, {}] 
     # print(len(config['dex_addrs']))
@@ -22,43 +22,99 @@ def performArbitrage():
         dex_info = contract.functions.getDEXinfo().call()
 
         # The DEX values are x, y, and k
-        dex_values[i]['x'] = dex_info[5] / (10**18)# ether liquidity in dex 
-        dex_values[i]['y'] = dex_info[6] / (10**10)# token liquidity in dex 
-        dex_values[i]['k'] = dex_info[4] / (10**28)
-        print(dex_values[i]['k'])
+        dex_values[i]['xd'] = dex_info[5] / (10**18) # ether liquidity in dex 
+        xd = dex_values[i]['xd']
+        dex_values[i]['yd'] = dex_info[6] / (10**10) # token liquidity in dex 
+        yd = dex_values[i]['yd']
+        dex_values[i]['kd'] = dex_info[4] / (10**28)
+        kd = dex_values[i]['kd']
+        
         # The current prices are pe and pt, the price of ETH and TC, respectively
         # should i make call to contract or use value in config file? 
-        dex_values[i]['p_e'] = config['price_eth'] # contract.functions.getEtherPrice().call() / 100
-        dex_values[i]['p_t'] = config['price_tc'] # contract.functions.getTokenPrice().call() / 100
+        dex_values[i]['pe'] = config['price_eth'] # contract.functions.getEtherPrice().call() / 100
+        pe = dex_values[i]['pe']
+        dex_values[i]['pt'] = config['price_tc'] # contract.functions.getTokenPrice().call() / 100
+        pt = dex_values[i]['pt']
 
         # The quantity of each that we currently have is qe and qt, for the quantity of ETH and TC, respectively
         # The values qe and qt are how much you (or, rather, the arbitrage.py program) have.
         dex_values[i]['qe'] = contract.functions.etherLiquidityForAddress(arbitrage_address).call() / (10**10)
+        qe = dex_values[i]['qe']
         dex_values[i]['qt'] = contract.functions.tokenLiquidityForAddress(arbitrage_address).call() / (10**10)
+        qt = dex_values[i]['qt'] 
+
+         # f is the percentage (out of 1.0) obtained after the DEX fees are removed
+        dex_values[i]['f'] = 1 - (dex_info[7] / dex_info[8]) # 1 - numerator/denominator
+        f = dex_values[i]['f']
+
+        # The gas fees, g, this is in units of ETH
+        dex_values[i]['g'] = 10  # Fix -------------------------------------------------------------------------------------------------------------------
+        g = dex_values[i]['g']
+        # eth.estimateGas
 
         # Our holdings are h_now (our current holdings) and h_after (our holdings after the transaction)
         # dollar amount of all the ETH and TC that it has
-        dex_values[i]['h_now'] = (dex_values[i]['qe'] * dex_values[i]['p_e'] + dex_values[i]['qt'] * dex_values[i]['p_t'])  
+        dex_values[i]['h_now'] = qe*pe + qt*pt 
+        h_now = dex_values[i]['h_now']
         
+        # put through profit maximiaztion function
+        # try trading in TC to receive eth 
+        # δt =  − yd± √ (f*kd*pe/pt)
+        δt1 = -yd + math.sqrt(f*kd*pe / pt) # how much token to trade in 
+        δt2 = -yd - math.sqrt(f*kd*pe / pt) 
+        # after holding formula: 
+        after_holdings_1 = (qe+f*xd-f*kd/(yd+δt1)) * pe + (qt-δt1) * pt - g * pe
+        after_holdings_2 = (qe+f*xd-f*kd/(yd+δt2)) * pe + (qt-δt2) * pt - g * pe
+
+        # try trading in eth to receive tc 
+        δe1 = -xd + math.sqrt(f*kd*pt / pe) 
+        δe2 = -xd - math.sqrt(f*kd*pt / pe) 
+        after_holdings_3 = (qt+f*yd-f*kd/(xd+δe1)) * pt + (qe-δe1) * pe - g * pe
+        after_holdings_4 = (qt+f*yd-f*kd/(xd+δe2)) * pt + (qe-δe2) * pe - g * pe
         
-        # put through profit maximiaztion function, then check if it wouldve made profit or not 
-        # try trading in TC 
-
-        dex_values[i]['h_after'] = ''
-
-        # The gas fees, g, this is in units of ETH
-
-
-        # f is the percentage (out of 1.0) obtained after the DEX fees are removed
-        dex_values[i]['f'] = 1 - (dex_info[7] / dex_info[8]) # 1 - numerator/denominator
-
+        greatest_after_holding = max(after_holdings_1, after_holdings_2, after_holdings_3, after_holdings_4)
+        # compare to before holding. 
+        if(greatest_after_holding > h_now):
+            print('profit exists')
+            dex_values[i]['h_after'] = greatest_after_holding
+            if(after_holdings_1 == greatest_after_holding):
+                dex_values[i]['tokenForEth'] = True 
+                dex_values[i]['token_amount'] = δt1
+                dex_values[i]['ether_amount'] = xd - (kd / (yd + δt1)) # etherLiquidity - (k / (tokenLiquidity + amountToken))
+            elif(after_holdings_2 == greatest_after_holding):
+                dex_values[i]['tokenForEth'] = True 
+                dex_values[i]['token_amount'] = δt2
+                dex_values[i]['ether_amount'] = xd - (kd / (yd + δt2))
+            elif(after_holdings_3 == greatest_after_holding):
+                dex_values[i]['tokenForEth'] = False 
+                dex_values[i]['token_amount'] = yd - (kd / (xd + δe1)) # tokenLiquidity - (k / (etherLiquidity + msg.value)); 
+                dex_values[i]['ether_amount'] = δe1
+            elif(after_holdings_4 == greatest_after_holding):
+                dex_values[i]['tokenForEth'] = False 
+                dex_values[i]['token_amount'] = yd - (kd / (xd + δe2))
+                dex_values[i]['ether_amount'] = δe2
+        else: 
+            print("no profit here")
+            dex_values[i]['amount'] = 0
+            dex_values[i]['tokenForEth'] = False 
+            dex_values[i]['h_after'] = 0    
         i += 1
+        # end for loop 
+        
+    # now compare the possible ending values of each dex. select the best one 
+    winning_dex_index = -1
+    best_trade_val = 0 #dex_values[i]['h_now']
+    i = 0
+    for x in dex_values:
+        if(x['h_after'] > best_trade_val):
+            best_trade_val = x[i]['h_after']
+            winning_dex_index = i
+        i+=1
+    if(winning_dex_index == -1):
+        # no dexes have profit 
+        output(0, 0, 0, 0)
+    else:
+        # output(ethAmt, tcAmt, fees, holdings)
+        output(dex_values[winning_dex_index]['ether_amount'], dex_values[winning_dex_index]['token_amount'], dex_values[winning_dex_index]['f'], dex_values[winning_dex_index]['h_after'])
 
-    # check if the holdings 
-
-    # profit formula: 
-    # ethAmountAfter * ethPrice + tcAmountAfter * tcPrice > ethAmountBefore * ethPrice + tcAmountBefore * tcPrice − gasFees
-    # eth.estimateGas
-
-    
 performArbitrage() 
